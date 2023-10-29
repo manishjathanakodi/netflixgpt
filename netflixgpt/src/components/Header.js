5e3c3f52d59d82fd214d56e56d4c10786508e6f4
manishjathanakodi <manishjathanakodi@gmail.com>
2023-10-29 13:39:43 +0530

    added assets

--- netflixgpt/src/components/Header.js
+++ netflixgpt/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import NetflixLogo from '../assets/Netflix_Logo_PMS.png'
 
 const Header = () => {
     return (
         <div>
             <div className='w-full absolute px-8 py-2 bg-gradient-to-b from-black to-transparent z-10'>
-                <img className='w-56' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='netflix-logo ' />
+                <img className='w-56' src={NetflixLogo} alt='netflix-logo ' />
             </div>
         </div>
 
--- netflixgpt/src/components/Login.js
+++ netflixgpt/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header'
-
+import background from '../assets/backgroundimg.jpg'
 
 const Login = () => {
     const [isSignUpForm, setIsSignUpForm] = React.useState(false)
@@ -12,7 +12,7 @@ const Login = () => {
             <Header />
 
             <div className='absolute'>
-                <img src='https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/fe1147dd-78be-44aa-a0e5-2d2994305a13/IN-en-20231016-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='body ' />
+                <img src={background} alt='body ' />
 
             </div>
             {/* <div className='flex justify-center items-center'> */}
